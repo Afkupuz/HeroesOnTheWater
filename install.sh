8192e28f24510ad844ba6735e81bf36dd69850d4
Afkupuz <nicolas@nirvana.local>
2016-07-01 14:23:04 -0700

    Feature: Requirements Installer
    run using ./install.sh from the command line (tested OS X)

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+meteor npm install --save angular angular-meteor
+meteor remove angular-templates
+meteor npm install --save-dev angular-mocks
+meteor npm install --save angular-ui-router
+meteor npm install --save angular-utils-pagination
+meteor npm install --save underscore
+meteor npm install --save angular-google-maps
+meteor npm install --save angular-simple-logger
+meteor npm install bootstrap@4.0.0-alpha.2 --save
+meteor npm uninstall bootstrap --save
+meteor npm install angular-animate
+meteor npm install angular-aria
+meteor npm install angular-material --save
+meteor npm install angular-ui-router --save
+meteor npm install gm --save
+meteor npm install ng-file-upload --save
+meteor npm install ng-img-crop --save
+meteor npm install angular-sortable-view --save
+meteor npm install ionic-sdk --save
+meteor npm install angular-sanitize --save
